7470ead1e05f87c78936d7e4ffa291cd31092a3f
lisasiahaan <108076005+lisasiahaan@users.noreply.github.com>
2022-06-27 05:15:24 +0700

    Add files via upload

---
+++ plugins/info-sc.js
@@ -0,0 +1,11 @@
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let str = `file script private kalau mau beli sc hubungi owner http://wa.me//6283167581315\n ,Terimakasih`
+conn.sendButtonDoc(m.chat, str, wm, 'Thanks','Bilek', ftextt, m)
+}
+handler.command = handler.help = ['sc']
+handler.tags = ['info']
+
+export default handler
